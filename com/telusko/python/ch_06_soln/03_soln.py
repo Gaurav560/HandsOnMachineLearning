b8b33c9b00a5e40e59c5f3de84c6b5ae99a5bc24
Gaurav Sharma <gsharma1154@gmail.com>
2024-10-18 02:22:15 +0530

    spam detection program in python

--- com/telusko/python/ch_06_soln/03_soln.py
+++ com/telusko/python/ch_06_soln/03_soln.py
@@ -1,1 +1,14 @@
-spam_comment=["Make a lot of money","buy now","subscribe this", "click this"]
+# List of keywords that are considered spam
+spam_keywords = ["Make a lot of money", "buy now", "subscribe this", "click this"]
+
+# Taking input from the user for a comment and converting it to lowercase for case-insensitive comparison
+spam_comments = input("Please enter your spam comment: ").lower()
+
+# Checking if any of the spam keywords are present in the comment
+# The 'any()' function returns True if any keyword from the spam_keywords list is found in spam_comments
+if any(keyword.lower() in spam_comments for keyword in spam_keywords):
+    # If a spam keyword is found in the comment, print "this is a spam"
+    print("This is a spam")
+else:
+    # If none of the keywords are found, print "this is not a spam"
+    print("This is not a spam")

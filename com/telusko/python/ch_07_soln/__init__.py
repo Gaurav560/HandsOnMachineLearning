bb9a1460235573b2ca792eae8012c1156307dd5c
Gaurav Sharma <gsharma1154@gmail.com>
2024-10-18 04:40:07 +0530

    for loop with else

---
+++ com/telusko/python/ch_07_soln/__init__.py
@@ -0,0 +1,22 @@
+#program to print from 1 to 50 in a while loop
+i=1
+while i<=50:
+    print(i)
+    i+=1
+
+#program to print the content of the list using while loops
+list1=["Harry","Gaurav",23,45.0]
+
+i=0
+while i<len(list1):
+    print(list1[i])
+    i+=1
+
+#using for loops
+for i in range(3,5):
+    print(i)
+
+#iterating a string-> same works for list and tuples also.
+str="gaurav"
+for i in str:
+    print(i)
---
+++ com/telusko/python/ch_07_soln/for_with_else.py
@@ -0,0 +1,5 @@
+l=[1,3,4,5,6]
+for i in l:
+     print(i)
+else:
+    print("done")

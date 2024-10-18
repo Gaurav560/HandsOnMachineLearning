1ba7be3e24faafa3088364eb65f1dd51a7e41a71
Gaurav Sharma <gsharma1154@gmail.com>
2024-10-19 01:57:07 +0530

    break, continue and pass

---
+++ com/telusko/python/ch_07_soln/break_continue_pass.py
@@ -0,0 +1,12 @@
+ #pass is a null statement in python . It instructs to do nothing.
+#i starts from zero and maximum range excluded.
+for i in range(6):
+
+    if i==3:
+        continue
+    print(i)
+
+#pass
+for i in range(6):
+    pass
+print("shaka")
